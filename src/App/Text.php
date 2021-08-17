ca5d739c1226c26e3f2125b8f30c8ddce2f5c320
pctcocom <pctco.com@gmail.com>
2021-08-17 13:40:54 +0800

    Update Text.php

--- src/App/Text.php
+++ src/App/Text.php
@@ -35,14 +35,25 @@ class Text{
    	$dir3 = substr($id, 5, 2);
 
       $this->dirs = $dir;
+
+      // /usr/local/var/www/website-ui/www/entrance
       $this->root = app()->getRootPath().'entrance';
 
+      // /uploads/books/000/00/00/90/
       $this->path = DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$dir1.DIRECTORY_SEPARATOR.$dir2.DIRECTORY_SEPARATOR.$dir3.DIRECTORY_SEPARATOR.substr($this->id, -2).DIRECTORY_SEPARATOR;
 
+      // /uploads/books/index.md
       $this->index = DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR.$this->dirs.DIRECTORY_SEPARATOR.'index'.$format;
 
+      // /usr/local/var/www/website-ui/www/entrance/uploads/books/000/00/00/90/
       $this->dir = $this->root.$this->path;
+
+      // /usr/local/var/www/website-ui/www/entrance/uploads/books/000/00/00/90/848656128.md
       $this->file = $this->dir.Skip::en($skip,substr($iid, -2)).$format;
+
+
+      // uploads/books/000/00/00/90/848656128.md
+      $this->files = str_replace($this->root.DIRECTORY_SEPARATOR,'',$this->file);
    }
    /**
    * @name get
@@ -50,10 +61,19 @@ class Text{
    * @return string
    **/
    public function get(){
-      if(file_exists($this->file)) {
-         return str_replace($this->root,'',$this->file);
-      } else {
-         return $this->index;
+      if ($this->config['os']['use'] == 1) {
+         $file = str_replace($this->root.DIRECTORY_SEPARATOR,'',$this->file);
+         if($this->storage->exist($file)) {
+            return $file;
+         } else {
+            return ltrim($this->index,DIRECTORY_SEPARATOR);
+         }
+      }else{
+         if(file_exists($this->file)) {
+            return str_replace($this->root,'',$this->file);
+         } else {
+            return $this->index;
+         }
       }
    }
    /**
@@ -62,9 +82,14 @@ class Text{
    * @return string
    **/
    public function post(){
-      $w = new FileWriter($this->file,'w+');
-      $w->write(self::GetContent());
-      return self::get();
+      if ($this->config['os']['use'] == 1) {
+         $file = str_replace($this->root.DIRECTORY_SEPARATOR,'',$this->file);
+         $this->storage->put($file,self::GetContent());
+      }else{
+         $w = new FileWriter($this->file,'w+');
+         $w->write(self::GetContent());
+         return self::get();
+      }
    }
    /**
    * @name delete
@@ -73,10 +98,16 @@ class Text{
    * @return string
    **/
    public function delete($range = ''){
-      // if ($this->config['os']['use'] == 1) {
-      //    return $this->storage->delete(str_replace($this->root,'',$this->file));
-      // }
+      if ($this->config['os']['use'] == 1) {
+         // 删除全部
+         if ($range === 'all') {
+            return $this->storage->delete(ltrim($this->path,DIRECTORY_SEPARATOR));
+         }
 
+         return $this->storage->delete($this->files);
+      }
+
+      // 删除全部
       if ($range === 'all') {
          $file = new File($this->dir);
          return $file->deleteAll();
@@ -85,15 +116,6 @@ class Text{
 
       $file = new File($this->file);
       return $file->delete();
-
-      // 删除全部
-      // $sourcePath = __DIR__ . '/tmp/';
-      //
-      // use Naucon\File\File;
-      // $fileObject = new File($sourcePath);
-      // $fileObject->deleteAll();
-
-
    }
    /**
    * @name get content
@@ -103,21 +125,31 @@ class Text{
    **/
    public function GetContent($to = false){
       if ($this->content === true) {
+         // 则复制 $this->index 路径文本内容
          $file = new FileReader($this->root.$this->index, 'r', true);
          $content = $file->read();
       }else if(substr(strrchr($this->content, '.'), 0) == $this->format){
-         $file = new FileReader($this->root.$this->content, 'r', true);
-         $content = $file->read();
+         // 获取自定义文件内容:/test/index.md
+         if ($this->config['os']['use'] == 1) {
+            $content = $this->storage->get(ltrim($this->content,DIRECTORY_SEPARATOR));
+         }else{
+            $file = new FileReader($this->root.$this->content, 'r', true);
+            $content = $file->read();
+         }
       }else if($this->content === 'self::get'){
-         $file = new FileReader($this->root.self::get(), 'r', true);
-         $content = $file->read();
+         // self::get:获取id路径下的文件内容
+         if ($this->config['os']['use'] == 1) {
+            $content = $this->storage->get(self::get());
+         }else{
+            $file = new FileReader($this->root.self::get(), 'r', true);
+            $content = $file->read();
+         }
       }else{
+         // 直接文本内容:'...'
          $content = $this->content;
       }
 
-      if ($to === false) {
-         return $content;
-      }
+      if ($to === false) return $content;
 
 
       if ($to === 'md-html') {
